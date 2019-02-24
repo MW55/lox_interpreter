3be541cdebca3d5f89b9859b2200333de6491a59
MW55 <marius.welzel@gmx.de>
2019-02-24 12:31:03 +0100

    Added ScanToken method and helper methods.

--- Lox/src/com/interpreter/lox/Scanner.java
+++ Lox/src/com/interpreter/lox/Scanner.java
@@ -29,11 +29,49 @@ class Scanner {
             scanToken();
         }
 
-       tokens.add(scanToken(EOF, "", null, line));
+        tokens.add(scanToken(EOF, "", null, line));
         return tokens;
     }
 
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    // Check if the loop reached the end of the source file.
     private boolean isAtEnd() {
         return current >= source.length();
     }
+
+    // Return the next character in the source file.
+    private char advance() {
+        current++;
+        return source.charAt(current -1);
+    }
+
+    // Create a new token for the current lexeme.
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    // For tokens with literal values.
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
 }
+
