3ec9cc0fc21d7e98f94d019f1c6acfe511b081d1
MW55 <marius.welzel@gmx.de>
2019-03-22 06:58:13 +0100

    Added evaluation of arithmetic operators and defined what is true and false.

--- Lox/src/com/interpreter/lox/Interpreter.java
+++ Lox/src/com/interpreter/lox/Interpreter.java
@@ -14,12 +14,20 @@ class Interpreter implements Expr.Visitor<Object> {
         Object right = evaluate(expr.right);
 
         switch (expr.operator.type) {
+            case BANG:
+                return !isTruthy(right);
             case MINUS:
                 return -(double)right;
         }
 
         // Unreachable.
         return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
     }
 
     // Evaluate parenthesis.
@@ -30,5 +38,30 @@ class Interpreter implements Expr.Visitor<Object> {
 
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case PLUS:
+                if (left instanceof Double && right instanceof Double) {
+                    return (double)left + (double)right;
+                }
+                if (left instanceof String && right instanceof String) {
+                    return (String)left + (String)right;
+                }
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        // Unreachable.
+        return null;
     }
 }
