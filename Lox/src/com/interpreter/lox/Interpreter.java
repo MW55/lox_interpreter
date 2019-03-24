e049fe0217f87d88d94ca53e43062ab410f33105
MW55 <marius.welzel@gmx.de>
2019-03-24 15:16:36 +0100

    Added runtime error reporting and made lox able to use the interpreter.

--- Lox/src/com/interpreter/lox/Interpreter.java
+++ Lox/src/com/interpreter/lox/Interpreter.java
@@ -4,6 +4,15 @@ import javax.management.RuntimeErrorException;
 
 class Interpreter implements Expr.Visitor<Object> {
 
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
+
     // Evaluate literals.
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -53,10 +62,25 @@ class Interpreter implements Expr.Visitor<Object> {
         return a.equals(b);
     }
 
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        // Hack. Work around Java adding ".0" to integer-valued doubles.
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+            }
+            return text;
+        }
+
+        return object.toString();
+    }
+
     // Evaluate parenthesis.
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
-        return evaluate(expr.expression)
+        return evaluate(expr.expression);
     }
 
     private Object evaluate(Expr expr) {
